49a2b7e33f0e7467e8c2403434ec6c15e8e7dd7b
Lucas <lcseferreira@hotmail.com>
2022-03-12 23:00:44 -0300

    Criação do TokenService para gerar um token

---
+++ Services/TokenService.cs
@@ -0,0 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using APIAuth.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace APIAuth.Services;
+
+public static class TokenService
+{
+    // Vai retornar o token
+    public static string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();   // Variável usada para gerar o token
+        var key = Encoding.ASCII.GetBytes(Settings.Secret); // Gera uma key através de um array de bytes
+
+        // Criação do token
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new System.Security.Claims.ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, user.Username), // User.Identity.Name
+                new Claim(ClaimTypes.Role, user.Role) // User.Identity.IsInRole
+
+            }),
+            Expires = DateTime.UtcNow.AddHours(8), // Vida útil do token
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature) // Credencial do token
+        };
+        // Gerando token
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace APIAuth
+{
+    public class Settings
+    {
+        public static string Secret = "das231d!dsa23!4@dsa2209#1as";
+    }
+}
